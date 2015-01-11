2f8f4e3a4888c75966d7df4a95f9dbbe524eac0e
xinchenpan <nathanpan@outlook.com>
2015-01-10 21:27:10 -0600

    <commit message>

--- plot1.R
+++ plot1.R
@@ -1,8 +1,8 @@
 library(data.table)
 consumption <- fread("household_power_consumption.txt")
 data <- consumption[66637:69516,]
-
+png(filename="plot1.png",width=480,height=480,pointsize = 12)
 hist(as.numeric(data$Global_active_power),main ="Global Active Power",
      xlab = "Global Active Power(kilowatts) ",col="red")
-dev.copy(png,file="plot1.png")
+
 dev.off()
--- plot4.R
+++ plot4.R
@@ -2,7 +2,8 @@ library(data.table)
 consumption <- fread("household_power_consumption.txt")
 data <- consumption[66637:69516,]
 date<-as.POSIXct(paste(data$Date,data$Time), format="%d/%m/%Y %H:%M:%S")
-par(mfrow=c(2,2),cex=0.6)
+png(filename="plot4.png",width=480,height=480,pointsize = 12)
+par(mfrow=c(2,2),cex=0.7)
 ##1
 plot(date, data$Global_active_power,type="n",xlab=" ",ylab="Global Active Power ")
 lines(date,data$Global_active_power)
@@ -14,12 +15,12 @@ plot(date,data$Sub_metering_1,type="n",xlab=" ",ylab="Energy sub metering ")
 lines(date,data$Sub_metering_1)
 lines(date,data$Sub_metering_2,col='red')
 lines(date,data$Sub_metering_3,col='blue')
-legend("topright", "(x,y)",lty=1,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black', 'red','blue' ),bty='n',cex=0.2)
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black', 'red','blue' ),bty='n',lwd=2.5)
 
 ##4
 plot(date,data$Global_reactive_power,type="n",xlab="datetime",ylab="Global reactive power ")
 lines(date,data$Global_reactive_power)
 
-dev.copy(png,file="plot4.png")
+
 dev.off()
 
