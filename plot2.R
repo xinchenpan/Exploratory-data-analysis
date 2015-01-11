b78ac45e8e694696b96cb3770cd06b5462a46d2e
xinchenpan <nathanpan@outlook.com>
2015-01-10 21:38:55 -0600

    <commit message>

--- plot2.R
+++ plot2.R
@@ -1,13 +1,13 @@
 library(data.table)
 consumption <- fread("household_power_consumption.txt")
 data <- consumption[66637:69516,]
-
+png(filename="plot2.png",width=480,height=480,pointsize = 12)
 
 date<-as.POSIXct(paste(data$Date,data$Time), format="%d/%m/%Y %H:%M:%S")
 
 plot(date, data$Global_active_power,type="n",xlab=" ",ylab="Global Active Power(kilowatts) ")
 lines(date,data$Global_active_power)
-dev.copy(png,file="plot2.png")
+
 dev.off()
 
 
